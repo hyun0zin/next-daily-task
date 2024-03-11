c59971aa2a857d4b18e45adf5f910aaa8c4c01bc
HyunjinKim <koujie11@gmail.com>
2024-03-11 18:04:58 +0900

    refact :code refactoring : 중복코드 하나의 컴포넌트로 분리하기

---
+++ src/app/(Rendering)/page.tsx
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// function RenderingPage({
+//   pageTitle,
+//   PageComponent,
+// }) {
+//   return (
+//     <div className="flex flex-col justify-center items-center">
+//       <h1 className="bg-indigo-100 ... w-48">
+//         {pageTitle} 페이지 렌더링입니다.
+//       </h1>
+//       <PageComponent />
+//     </div>
+//   );
+// }
+
+// export default RenderingPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,45 @@
 import Header from "@/components/Header";
 
 import Image from "next/image";
+// import RenderingPage from "./(Rendering)/page";
+import { useRouter } from "next/router";
+
+// interface HomeProps {
+//   pageTitle: string;
+//   PageComponent: React.ComponentType<any>;
+// }
 
 export default function Home() {
-  return <div className="m-4">원하는 렌더링 방식 버튼을 눌러주세요.</div>;
+  // const router = useRouter();
+  // const { page } = router.query;
+
+  // let pageTitle: string, PageComponent: React.ComponentType<any>;
+  // switch (page) {
+  //   case "SSG":
+  //     pageTitle: "SSG 페이지 렌더링입니다.";
+  //     PageComponent: () => import("@/components/rendering/SSG");
+  //     break;
+  //   case "ISR":
+  //     pageTitle: "ISR 페이지 렌더링입니다.";
+  //     PageComponent: () => import("@/components/rendering/ISR");
+  //     break;
+  //   case "SSR":
+  //     pageTitle: "SSR 페이지 렌더링입니다.";
+  //     PageComponent: () => import("@/components/rendering/SSR");
+  //     break;
+  //   case "CSR":
+  //     pageTitle: "CSR 페이지 렌더링입니다.";
+  //     PageComponent: () => import("@/components/rendering/CSR");
+  //     break;
+  //   default:
+  //     pageTitle: "Page Not Found";
+  //     PageComponent: () => <div>Page Not Found</div>;
+  // }
+
+  return (
+    <div className="m-4">
+      <p>원하는 렌더링 방식 버튼을 눌러주세요.</p>
+      {/* <RenderingPage pageTitle={pageTitle} PageComponent={PageComponent} /> */}
+    </div>
+  );
 }
